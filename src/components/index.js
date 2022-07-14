a9fe69149728ca99863941e43c10c70f4136cf82
a7mdmo74 <a7mdmo74@gmail.com>
2022-07-14 19:52:17 +0200

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as About } from "./About/About";
+export { default as AllInOne } from "./AllInOne/AllInOne";
+export { default as Footer } from "./Footer/Footer";
+export { default as Hero } from "./Hero/Hero";
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Pricing } from "./Pricing/Pricing";
+export { default as Support } from "./Support/Support";
